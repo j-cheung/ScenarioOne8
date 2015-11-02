dd793fbf5089c6c78260d92c848777fe41bacdeb
stupidjc <stupidjc@Johnsons-MacBook-Pro.local>
2015-11-02 13:40:31 +0000

    created first view

--- toDoList/toDoApp/views.py
+++ toDoList/toDoApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. We are team 8, we all obey Anita")
 
 # Create your views here.
+
--- toDoList/toDoList/urls.py
+++ toDoList/toDoList/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^toDo/', include('toDoApp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
