f283920592b42d78149d89ad68e2369b65cf9928
stupidjc <stupidjc@dhcp-21-124.xlate.eduroam.ucl.ac.uk>
2015-11-03 16:17:43 +0000

    linked login n stuff

--- toDoList/toDoApp/forms.py
+++ toDoList/toDoApp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from toDoApp.models import List, Task
+from django.contrib.auth.models import User
 
 class newListForm(forms.ModelForm):
 
@@ -12,3 +13,15 @@ class newTaskForm(forms.ModelForm):
 	class Meta:
 		model = Task
 		fields = ('task_title', 'description', )
+
+
+
+class UserForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password')
+
+
+class LoginForm(forms.Form):
+		username = forms.CharField(label = 'Username', max_length=20)
+		password = forms.CharField(max_length=32, widget=forms.PasswordInput)
--- toDoList/toDoApp/views.py
+++ toDoList/toDoApp/views.py
@@ -6,29 +6,68 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 from toDoApp.models import List, Task
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
-from .forms import newListForm, newTaskForm
+from .forms import newListForm, newTaskForm, UserForm, LoginForm 
+
+def adduser(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            return HttpResponseRedirect('/loginuser/')
+    else:
+        form = UserForm() 
+
+    return render(request, 'toDoApp/adduser.html', {'form': form})
+
+def loginuser(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/user/')
+            else:
+                return HttpResponse("Invalid login")
+    else:
+        form = LoginForm()
+
+    return render(request, 'toDoApp/loginuser.html', {'form': form})
+
+
+def logoutuser(request):
+    logout(request)
+    return HttpResponseRedirect('/loginuser/')
+
 
 class UserListView(ListView):
 	#template_name = "user_list.html"
 	model = List
 
+#	@method_decorator(login_required)
 	def get_queryset(self):
-		#currentUser = request.user
-		currentUser = User.objects.get(username = 'test123')
+		currentUser = self.request.user
+		#currentUser = User.objects.get(username = 'test123')
 		userLists = List.objects.filter(user = currentUser)
 		#userLists = List.objects.all()
 		return userLists
 
+@login_required
 def createList(request):
 	if request.method == 'POST':
 		form = newListForm(request.POST)
 		if form.is_valid():
 			newList = form.save(commit=False)
-			newListUser = User.objects.get(username = 'test123')
-			newList.user = newListUser
+			#newListUser = request.user
+			newList.user = request.user
 			newList.save()
-			return HttpResponseRedirect('/lists/')
+			return HttpResponseRedirect('/user/')
 	else:
 		form = newListForm()
 
@@ -39,10 +78,11 @@ def createList(request):
 #	model = List
 #	fields = ['list_title']
 
+
 class TaskListView(ListView):
 	template_name = "toDoApp/task_list.html"
 	#model = Task
-
+#	@method_decorator(login_required)
 	def get_queryset(self):
 		self.list = get_object_or_404(List, id=self.args[0])
 		return Task.objects.filter(theList_id = self.list)
@@ -51,6 +91,7 @@ class TaskListView(ListView):
 		#listTasks = Task.objects.all()
 		#return listTasks
 
+@login_required
 def createTask(request, listID):
 	if request.method == 'POST':
 		form = newTaskForm(request.POST)
