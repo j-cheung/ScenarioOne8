6bdce38dc324764cf464103caeb52367890d12ca
stupidjc <stupidjc@dhcp-26-187.xlate.eduroam.ucl.ac.uk>
2015-11-06 14:00:35 +0000

    minor error

--- scripts/pullUpdatesProduction.sh
+++ scripts/pullUpdatesProduction.sh
@@ -2,7 +2,7 @@ sudo systemctl stop httpd
 
 cd ~/Desktop/ScenarioOne8
 git pull
-cp ~/Desktop/ScenarioOne8/scripts/pullUpdates.sh ~/
+cp ~/Desktop/ScenarioOne8/scripts/pullUpdatesProduction.sh ~/
 
 ssh localuser@studvm93-p "sh restorescript-main.sh" 
 
