c0658ffc1b14793cc151fe5dd240e34f4811a236
stupidjc <stupidjc@dhcp-26-187.xlate.eduroam.ucl.ac.uk>
2015-11-06 13:45:56 +0000

    sshception

--- scripts/pullUpdates.sh
+++ scripts/pullUpdates.sh
@@ -2,6 +2,8 @@ sudo systemctl stop httpd
 
 cd ~/Desktop/ScenarioOne8
 git pull
+cp ~/Desktop/ScenarioOne8/scripts/pullUpdates.sh ~/
 
+ssh localuser@studvm93-p "sh restorescript-main.sh" 
 
 sudo systemctl start httpd
